2348b573ee503f9cb32dc59cce17559597024888
unknown <hyeono2@naver.com>
2020-05-10 19:54:27 +0900

    deploy 설정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { Header, SideMenu, ContentWrapper } from './components';
-import { BrowserRouter as Router } from 'react-router-dom';
+
 
 class App extends Component{
   render(){
     return (
       <div>
-        <Router basename={process.env.PUBLIC_URL}>
+        {/* <Router basename={process.env.PUBLIC_URL}> */}
           {/* <Header></Header> */}
           <SideMenu></SideMenu>
           <ContentWrapper></ContentWrapper>
-        </Router>
+        {/* </Router> */}
       </div>
     );
   }
